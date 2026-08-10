e6ef997cbc88b3c361ba46ae565cade4ca746c70
tennsinnhann <aguteki177@docomo.ne.jp>
2026-08-10 14:07:33 +0900

    材料在庫の追加・保存機能を実装

--- js/stock-list.js
+++ js/stock-list.js
@@ -5,3 +5,93 @@ if(backButton){
         window.location.href = "index.html";
     });
 }
+
+const STOCK_STORAGE_KEY = "stockList";
+
+let stockList = 
+    JSON.parse(localStorage.getItem(STOCK_STORAGE_KEY)) || [];
+
+const stockNameInput = 
+    document.getElementById("stockNameInput");
+
+const stockAmountInput = 
+    document.getElementById("stockAmountInput");
+
+const addStockButton = 
+    document.getElementById("addStockButton")
+
+const stockListElement = 
+    document.getElementById("stockList");
+
+const emptyMessage = 
+    document.getElementById("emptyMessage");
+
+/**
+ * 保存用関数
+ */
+function saveStockList(){
+    localStorage.setItem(
+        STOCK_STORAGE_KEY,
+        JSON.stringify(stockList)
+    );
+}
+
+/**
+ * 表示用関数
+ */
+function renderStockList(){
+    stockListElement.innerHTML = "";
+
+    if(stockList.length === 0){
+        emptyMessage.style.display = "block";
+        return;
+    }
+
+    emptyMessage.style.display = "none";
+
+    stockList.forEach((item) =>{
+        const li = document.createElement("li");
+
+        const name = document.createElement("span");
+        name.textContent = item.name;
+
+        const amount = document.createElement("span");
+        amount.textContent = item.amount;
+
+        li.appendChild(name);
+        li.appendChild(amount);
+
+        stockListElement.appendChild(li);
+    });
+}
+
+/**
+ * 追加ボタン
+ */
+addStockButton.addEventListener("click", ()=>{
+    const name = stockNameInput.value.trim();
+    const amount = stockAmountInput.value.trim();
+
+    if(name === ""){
+        alert("材料名を入力してください");
+        return;
+    }
+
+    if(amount === ""){
+        alert("在庫量を入力してください");
+        return;
+    }
+
+    stockList.push({
+        name: name,
+        amount: amount
+    });
+
+    saveStockList();
+    renderStockList();
+
+    stockNameInput.value = "";
+    stockAmountInput.value = "";
+});
+
+renderStockList();
