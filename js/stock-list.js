ded447bf1a0397909444bf4a88cda50e992b96dc
tennsinnhann <aguteki177@docomo.ne.jp>
2026-08-10 13:34:57 +0900

    材料在庫管理画面の土台を追加

---
+++ js/stock-list.js
@@ -0,0 +1,7 @@
+const backButton = document.getElementById("backButton");
+
+if(backButton){
+    backButton.addEventListener("click", () =>{
+        window.location.href = "index.html";
+    });
+}
